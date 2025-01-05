713676271b79585a71b4328c1d44fa0d47f09dba
Tatiana Manczak <tatsiana_rogalska@wp.pl>
2025-01-05 13:32:12 +0100

    add generateOdds function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -347,8 +347,10 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  if (!len) return [];
+  const arr = Array.from({ length: len });
+  return arr.map((item, index) => 2 * index + 1);
 }
 
 /**
