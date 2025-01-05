bf18888a6311c354377f73abd5f2d228fa1afce9
Tatiana Manczak <tatsiana_rogalska@wp.pl>
2025-01-05 13:41:57 +0100

    add getFalsyValsCount function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -381,8 +381,9 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const result = arr.filter((item) => Boolean(item));
+  return result.length;
 }
 
 /**
