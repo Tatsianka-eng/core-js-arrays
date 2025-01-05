d63e2bd8008137f874afa2d571d3b7acf61d5a2b
Tatiana Manczak <tatsiana_rogalska@wp.pl>
2025-01-05 12:47:15 +0100

    fix a fixed solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -21,7 +21,7 @@
  *    getIntervalArray(3, 3) => [ 3 ]
  */
 function getIntervalArray(start, end) {
-  const arr = Array.from({length: end - start + 1});
+  const arr = Array.from({ length: end - start + 1 });
   return arr.map((item, index) => start + index);
 }
 
