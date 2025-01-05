361a8921a2e9511e77721f54aa21a5081aacab2c
Tatiana Manczak <tatsiana_rogalska@wp.pl>
2025-01-05 15:08:07 +0100

    add calculateBalance function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -315,8 +315,12 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  const flatArr = arr.flat();
+  return flatArr.reduce((acc, item, index) => {
+    if (!(index % 2)) acc += item;
+    if (index % 2) acc -= item;
+  }, 0); 
 }
 
 /**
